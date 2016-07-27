1a2a44388cecc53292e886d14e12d2ec7a86c8f2
David Gnedt <david.gnedt@jku.at>
2016-07-27 14:28:39 +0200

    Build CExt with Sulong during bundle installation

--- ext/argon2_wrap/extconf.rb
+++ ext/argon2_wrap/extconf.rb
@@ -1,1 +1,8 @@
-#I must admit I have no understanding of why this empty file works.
+JRUBY_HOME = ENV['JRUBY_HOME']
+
+if JRUBY_HOME
+	GEM_HOME = File.expand_path('../../..', __FILE__)
+	ENV['GEM_HOME'] = GEM_HOME
+
+	system(File.join(JRUBY_HOME, 'tool/jt.rb') + ' cextc ' + GEM_HOME)
+end
