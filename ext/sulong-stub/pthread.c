bd3fb1e056cfd5f59aeb6b8ccf04a824d3a5d0d6
David Gnedt <david.gnedt@jku.at>
2016-08-30 10:13:35 +0200

    Update pthread stub for OSX

--- ext/sulong-stub/pthread.c
+++ ext/sulong-stub/pthread.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "pthread.h"
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-parameter"
+
 int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg) {
   static int warn = 1;
   if (warn) {
@@ -17,3 +20,5 @@ void pthread_exit(void *retval) {
 int pthread_join(pthread_t thread, void **retval) {
   return 0;
 }
+
+#pragma clang diagnostic pop
